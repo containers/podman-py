e19690ecb5b2802ba9380c2cdb67bab69588e185
Brandon Marlow <brandon.n.marlow@gmail.com>
2020-05-23 14:51:56 -0700

    add def for system/get_info
    Signed-off-by: Brandon Marlow <brandon.n.marlow@gmail.com>

--- podman/system/__init__.py
+++ podman/system/__init__.py
@@ -1,6 +1,9 @@
 """Provide system level information for the Podman service."""
 from http import HTTPStatus
+import json
+import logging
 
+import podman.errors as errors
 
 def version(api, verify_version=False):
     """Obtain a dictionary of versions for the Podman components."""
@@ -11,5 +14,17 @@ def version(api, verify_version=False):
     # TODO: verify api.base and header[Api-Version] compatible
     return {}
 
+def get_info(api):
+    """Returns information on the system and libpod configuration"""
+    try:
+        response = api.request("GET", "/info")
+        return json.loads(str(response.read(), 'utf-8'))
+    except errors.NotFoundError as e:
+        _report_not_found(e, e.response)
+
+def _report_not_found(e, response):
+    body = json.loads(response.read())
+    logging.info(body["cause"])
+    raise errors.ImageNotFound(body["message"]) from e
 
 __all__ = ["version"]
