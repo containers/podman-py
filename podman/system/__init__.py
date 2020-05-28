12022f492f2cafee2cc3e0a475134ff0a17c0d36
Brandon Marlow <brandon.n.marlow@gmail.com>
2020-05-28 10:58:53 -0700

    update system calls based on API docs
    Signed-off-by: Brandon Marlow <brandon.n.marlow@gmail.com>

--- podman/system/__init__.py
+++ podman/system/__init__.py
@@ -1,12 +1,8 @@
 """Provide system level information for the Podman service."""
 from http import HTTPStatus
 import json
-import logging
 
-import podman.errors as errors
-
-
-def version(api, verify_version=False):
+def ping(api, verify_version=False):
     """Obtain a dictionary of versions for the Podman components."""
     response = api.request("GET", "/_ping")
     response.read()
@@ -18,29 +14,42 @@ def version(api, verify_version=False):
 
 def get_info(api):
     """Returns information on the system and libpod configuration"""
-    try:
-        response = api.request("GET", "/info")
-        return json.loads(str(response.read(), 'utf-8'))
-    except errors.NotFoundError as e:
-        _report_not_found(e, e.response)
-
+    path = api.join("/info")
+    response = api.request("GET", path)
+    return json.loads(response.read())
 
 def show_disk_usage(api):
-    """Return information about disk usage for containers, images and volumes"""
-    try:
-        response = api.request("GET", "/system/df")
-        return json.loads(str(response.read(), 'utf-8'))
-    except errors.NotFoundError as e:
-        _report_not_found(e, e.response)
+    """Return information about disk usage for containers, images, and volumes"""
+    # this endpoint is non-functional (returns 0 values) podman 1.9.2
+    # uri does not conform to the /{version}/lipod/{endpoint} format
+    response = api.request("GET", "/system/df")
+    return json.loads(response.read())
 
+def get_events(api):
+    """Returns events filtered on query parameters"""
+    # this endpoint is non-functional and will just hang podman 1.9.2
+    path = api.join("/events")
+    response = api.request("GET", path)
+    return json.loads(response.read())
 
-def _report_not_found(e, response):
-    body = json.loads(response.read())
-    logging.info(body["cause"])
-    raise errors.ImageNotFound(body["message"]) from e
+def prune_unused_data(api):
+    """Prune unused data"""
+    # this endpoint is non-functional podman 1.9.2
+    path = api.join("/system/prune")
+    response = api.request("POST", path)
+    return  json.loads(response.read())
+
+def version(api):
+    """Get system version information"""
+    # uri does not conform to the /{version}/lipod/{endpoint} format
+    response = api.request("GET", "/version")
+    return json.loads(response.read())
 
 __all__ = [
-    "version",
+    "ping",
     "get_info",
     "show_disk_usage",
+    "get_events",
+    "prune_unused_data",
+    "version",
 ]
