14795f5e159c529e63b2bec0d5f8196981298369
Brandon Marlow <brandon.n.marlow@gmail.com>
2020-05-28 11:22:15 -0700

    add helper function
    Signed-off-by: Brandon Marlow <brandon.n.marlow@gmail.com>

--- podman/system/__init__.py
+++ podman/system/__init__.py
@@ -2,8 +2,15 @@
 from http import HTTPStatus
 import json
 
+def make_call(api, endpoint, method='GET', params=None):
+    """A helper function to keep things DRY"""
+    path = api.join(endpoint)
+    response = api.request(method, path)
+    return response.read()
+
 def ping(api, verify_version=False):
     """Obtain a dictionary of versions for the Podman components."""
+    # this is hitting the compatability endpoint
     response = api.request("GET", "/_ping")
     response.read()
     if response.getcode() == HTTPStatus.OK:
@@ -11,39 +18,29 @@ def ping(api, verify_version=False):
     # TODO: verify api.base and header[Api-Version] compatible
     return {}
 
-
 def get_info(api):
     """Returns information on the system and libpod configuration"""
-    path = api.join("/info")
-    response = api.request("GET", path)
-    return json.loads(response.read())
+    return json.loads(make_call(api, "/info"))
 
 def show_disk_usage(api):
     """Return information about disk usage for containers, images, and volumes"""
-    # this endpoint is non-functional (returns 0 values) podman 1.9.2
-    # uri does not conform to the /{version}/lipod/{endpoint} format
-    response = api.request("GET", "/system/df")
-    return json.loads(response.read())
+    # this endpoint is non-functional podman 1.9.2
+    return json.loads(make_call(api, "/system/df"))
 
 def get_events(api):
     """Returns events filtered on query parameters"""
     # this endpoint is non-functional and will just hang podman 1.9.2
-    path = api.join("/events")
-    response = api.request("GET", path)
-    return json.loads(response.read())
+    return json.loads(make_call(api, "/events"))
 
 def prune_unused_data(api):
     """Prune unused data"""
     # this endpoint is non-functional podman 1.9.2
-    path = api.join("/system/prune")
-    response = api.request("POST", path)
-    return  json.loads(response.read())
+    return json.loads(make_call(api, "/system/prune", "POST"))
 
 def version(api):
     """Get system version information"""
     # uri does not conform to the /{version}/lipod/{endpoint} format
-    response = api.request("GET", "/version")
-    return json.loads(response.read())
+    return json.loads(make_call(api, "/version"))
 
 __all__ = [
     "ping",
