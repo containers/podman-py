2190e7a2575c811f8c00ac5ef0dd991b779415e9
Urvashi Mohnani <umohnani@redhat.com>
2024-01-03 07:35:28 -0500

    Bump version to 4.8.2
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

--- podman/tests/__init__.py
+++ podman/tests/__init__.py
@@ -3,5 +3,5 @@
 # Do not auto-update these from version.py,
 #   as test code should be changed to reflect changes in Podman API versions
 BASE_SOCK = "unix:///run/api.sock"
-LIBPOD_URL = "http://%2Frun%2Fapi.sock/v4.8.1/libpod"
+LIBPOD_URL = "http://%2Frun%2Fapi.sock/v4.8.2/libpod"
 COMPATIBLE_URL = "http://%2Frun%2Fapi.sock/v1.40"
