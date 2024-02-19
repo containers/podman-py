b190fc928d18646d7c2f32e71be105f6d3dfcc73
Urvashi Mohnani <umohnani@redhat.com>
2024-02-19 10:11:24 -0500

    Use new json connections file
    Podman has updated where it will store its
    system connection information to a new json
    format file.
    Add support to podman-py to read from both the
    new json file and old toml file giving preference
    to the new json file.
    
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

--- podman/domain/config.py
+++ podman/domain/config.py
@@ -4,6 +4,7 @@ import sys
 import urllib
 from pathlib import Path
 from typing import Dict, Optional
+import json
 
 import xdg.BaseDirectory
 
@@ -48,12 +49,16 @@ class ServiceConnection:
     @cached_property
     def url(self):
         """urllib.parse.ParseResult: Returns URL for service connection."""
-        return urllib.parse.urlparse(self.attrs.get("uri"))
+        if self.attrs.get("uri"):
+            return urllib.parse.urlparse(self.attrs.get("uri"))
+        return urllib.parse.urlparse(self.attrs.get("URI"))
 
     @cached_property
     def identity(self):
         """Path: Returns Path to identity file for service connection."""
-        return Path(self.attrs.get("identity"))
+        if self.attrs.get("identity"):
+            return Path(self.attrs.get("identity"))
+        return Path(self.attrs.get("Identity"))
 
 
 class PodmanConfig:
@@ -62,17 +67,45 @@ class PodmanConfig:
     def __init__(self, path: Optional[str] = None):
         """Read Podman configuration from users XDG_CONFIG_HOME."""
 
+        self.is_default = False
         if path is None:
             home = Path(xdg.BaseDirectory.xdg_config_home)
-            self.path = home / "containers" / "containers.conf"
+            self.path = home / "containers" / "podman-connections.json"
+            old_toml_file = home / "containers" / "containers.conf"
+            self.is_default = True
+        # this elif is only for testing purposes
+        elif "@@is_test@@" in path:
+            test_path = path.replace("@@is_test@@", '')
+            self.path = Path(test_path) / "podman-connections.json"
+            old_toml_file = Path(test_path) / "containers.conf"
+            self.is_default = True
         else:
             self.path = Path(path)
 
         self.attrs = {}
         if self.path.exists():
-            with self.path.open(encoding='utf-8') as file:
+            try:
+                with open(self.path, encoding='utf-8') as file:
+                    self.attrs = json.load(file)
+            except:  # pylint: disable=bare-except
+                # if the user specifies a path, it can either be a JSON file
+                # or a TOML file - so try TOML next
+                try:
+                    with self.path.open(encoding='utf-8') as file:
+                        buffer = file.read()
+                    loaded_toml = toml_loads(buffer)
+                    self.attrs.update(loaded_toml)
+                except Exception as e:
+                    raise AttributeError(
+                        "The path given is neither a JSON nor a TOML connections file"
+                    ) from e
+
+        # Read the old toml file configuration
+        if self.is_default and old_toml_file.exists():
+            with old_toml_file.open(encoding='utf-8') as file:
                 buffer = file.read()
-            self.attrs = toml_loads(buffer)
+            loaded_toml = toml_loads(buffer)
+            self.attrs.update(loaded_toml)
 
     def __hash__(self) -> int:
         return hash(tuple(self.path.name))
@@ -98,9 +131,21 @@ class PodmanConfig:
         """
         services: Dict[str, ServiceConnection] = {}
 
+        # read the keys of the toml file first
         engine = self.attrs.get("engine")
         if engine:
             destinations = engine.get("service_destinations")
+            for key in destinations:
+                connection = ServiceConnection(key, attrs=destinations[key])
+                services[key] = connection
+
+        # read the keys of the json file next
+        # this will ensure that if the new json file and the old toml file
+        # has a connection with the same name defined, we always pick the
+        # json one
+        connection = self.attrs.get("Connection")
+        if connection:
+            destinations = connection.get("Connections")
             for key in destinations:
                 connection = ServiceConnection(key, attrs=destinations[key])
                 services[key] = connection
@@ -111,11 +156,18 @@ class PodmanConfig:
     def active_service(self):
         """Optional[ServiceConnection]: Returns active connection."""
 
+        # read the new json file format
+        connection = self.attrs.get("Connection")
+        if connection:
+            active = connection.get("Default")
+            destinations = connection.get("Connections")
+            return ServiceConnection(active, attrs=destinations[active])
+
+        # if we are here, that means there was no default in the new json file
         engine = self.attrs.get("engine")
         if engine:
             active = engine.get("active_service")
             destinations = engine.get("service_destinations")
-            for key in destinations:
-                if key == active:
-                    return ServiceConnection(key, attrs=destinations[key])
+            return ServiceConnection(active, attrs=destinations[active])
+
         return None
