281508bccbd17730133a08c1295a7a4e0e819457
Milan Balazs <milanbalazs01@gmail.com>
2024-07-17 16:23:35 +0200

    Fix the Black formatting issues
    Signed-off-by: Milan Balazs <milanbalazs01@gmail.com>

--- podman/domain/images_manager.py
+++ podman/domain/images_manager.py
@@ -209,9 +209,7 @@ class ImagesManager(BuildMixin, Manager):
 
         headers = {
             # A base64url-encoded auth configuration
-            "X-Registry-Auth": encode_auth_header(auth_config)
-            if auth_config
-            else ""
+            "X-Registry-Auth": encode_auth_header(auth_config) if auth_config else ""
         }
 
         params = {
@@ -298,9 +296,7 @@ class ImagesManager(BuildMixin, Manager):
 
         headers = {
             # A base64url-encoded auth configuration
-            "X-Registry-Auth": encode_auth_header(auth_config)
-            if auth_config
-            else ""
+            "X-Registry-Auth": encode_auth_header(auth_config) if auth_config else ""
         }
 
         params = {
