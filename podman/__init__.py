b5718c7545067d6ed6df5d7bcf1d5167d7a03fdc
Nicola Sella <nsella@redhat.com>
2024-12-02 16:58:54 +0100

    Remove code that is never executed
    Python Version is already set to be < 3.9 so this code will never run
    
    Signed-off-by: Nicola Sella <nsella@redhat.com>

--- podman/__init__.py
+++ podman/__init__.py
@@ -1,12 +1,7 @@
 """Podman client module."""
 
-import sys
-
 from podman.client import PodmanClient, from_env
 from podman.version import __version__
-
-if sys.version_info < (3, 9):
-    raise ImportError("Python 3.6 or greater is required.")
 
 # isort: unique-list
 __all__ = ['PodmanClient', '__version__', 'from_env']
